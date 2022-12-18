54a4799fbe3309522b9664df51be60e672e16dec
RajatVijaywargiya <rajat.vijaywargiya@gmail.com>
2022-12-19 01:38:15 +0530

    added trace

--- src/main/java/utilities/PageSetUp.java
+++ src/main/java/utilities/PageSetUp.java
@@ -1,18 +1,28 @@
 package utilities;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserType;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.*;
 
 public class PageSetUp {
     private static Page page;
+    private static Browser browser;
+
+    private static BrowserContext browserContext;
     private static Playwright getPlaywright() {
         return Playwright.create();
     }
 
     private static Browser getBrowser() {
-        return getPlaywright().chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
+        if (browser == null) {
+            browser = getPlaywright().chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
+        }
+        return browser;
+    }
+
+    public static BrowserContext getBrowserContext() {
+        if (browserContext == null) {
+            browserContext = getBrowser().newContext();
+        }
+        return browserContext;
     }
 
     public static Page getPage() {
--- src/test/java/steps/BaseTest.java
+++ src/test/java/steps/BaseTest.java
@@ -1,21 +1,29 @@
 package steps;
 
 
+import com.microsoft.playwright.BrowserContext;
 import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Tracing;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import utilities.PageSetUp;
 import utilities.PropertyLoader;
 
 import java.io.IOException;
+import java.nio.file.Paths;
 
 public class BaseTest extends PageSetUp{
 
     private final Page page = getPage();
+    private final BrowserContext browserContext = getBrowserContext();
 
     @Before
     public void setUp() {
         try {
+            browserContext.tracing().start(new Tracing.StartOptions()
+                    .setScreenshots(true)
+                    .setSnapshots(true)
+                    .setSources(false));
             page.navigate(new PropertyLoader().getPropertyFromConfig("url"));
             System.out.println(page.title());
         } catch (IOException e) {
@@ -23,8 +31,9 @@ public class BaseTest extends PageSetUp{
         }
     }
 
-//    @After
-//    public void tearDown() {
-//        page.close();
-//    }
+    @After
+    public void tearDown() {
+        browserContext.tracing().stop(new Tracing.StopOptions()
+                .setPath(Paths.get("trace.zip")));
+    }
 }
